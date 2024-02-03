7756fd0c5f9207edab22bb41acefe162d305daf9
jip002 <jip002@uscd.edu>
2024-02-03 13:17:20 -0800

    new jihun branch commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import schoolListData from './data/schoolList.json';
 
 function App() {
+  const [schools, setSchools] = useState([]);
+
+  useEffect(() => {
+    setSchools(schoolListData);
+  }, []);
+
   return (
     <div className="App">
-      
+      <h1>K-Time</h1>
+      <div className = "schoolListContainer">
+        {schools.map(school => (
+          <div key={school.id}>{school.name}</div>
+        ))}
+      </div>
     </div>
   );
 }
