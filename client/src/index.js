40a39e732333c2c40e0da50ad0ed539e969cb79a
jameslol417 <jameslol4177@gmail.com>
2024-02-17 14:15:10 -0800

    implemented rudimentary login

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="169058009085-6bipqtnogbijdkd7c4ro0oa16i9b34vg.apps.googleusercontent.com">
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </GoogleOAuthProvider>,
 );
 
 reportWebVitals();
