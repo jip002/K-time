11677527e40a763a9c4d24e77aef5810f0cef868
jameslol417 <jameslol4177@gmail.com>
2024-02-27 02:56:33 -0800

    temp

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -11,87 +11,96 @@ import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 
 
 export const Login = () => {
-//   const [ user, setUser ] = useState([]);
-//   const [ profile, setProfile ] = useState([]);
+  const [ user, setUser ] = useState([]);
+  const [ profile, setProfile ] = useState([]);
+  const navigate = useNavigate();
 
-//   const login = useGoogleLogin({
-//       onSuccess: (codeResponse) => setUser(codeResponse),
-//       onError: (error) => console.log('Login Failed:', error)
-//   });
+  const login = useGoogleLogin({
+      onSuccess: (codeResponse) => setUser(codeResponse),
+      onError: (error) => console.log('Login Failed:', error)
+  });
 
-//    useEffect(
-//       () => {
-//           if (user) {
-//             //   console.log(user);
-//               axios
-//                   .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-//                       headers: {
-//                           Authorization: `Bearer ${user.access_token}`,
-//                           Accept: 'application/json'
-//                       }
-//                   })
-//                   .then((res) => {
-//                       setProfile(res.data);
-//                       sendToServer(res.data);
-//                         navigate('/forum', {
-//                             state: {
-//                                 name: res.data.name,
-//                                 email: res.data.email,
-//                                 picture: res.data.picture
-//                             }
-//                         });
-//                   })
-//                   .catch((err) => console.log(err));
-//           }
-//       },
-//       [ user ]
-//   );
-//   // log out function to log the user out of google and set the profile array to null
-//   const logOut = () => {
-//       googleLogout();
-//       setProfile(null);
-//   };
+   useEffect(
+      () => {
+          if (user) {
+            //   console.log(user);
+              axios
+                  .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                      headers: {
+                          Authorization: `Bearer ${user.access_token}`,
+                          Accept: 'application/json'
+                      }
+                  })
+                  .then((res) => {
+                      console.log(res.data);
+                      setProfile(res.data);
+                      sendToServer(res.data);
+                        navigate('/login', {
+                            state: {
+                                name: res.data.name,
+                                email: res.data.email,
+                                // picture: res.data.picture
+                            }
+                        });
+                  })
+                  .catch((err) => console.log(err));
+          }
+      },
+      [ user ]
+  );
+  // log out function to log the user out of google and set the profile array to null
+  const logOut = () => {
+      googleLogout();
+      setProfile(null);
+  };
 
-//   const sendToServer = (userInfo) => {
-//     const userData = {
-//         name: userInfo.name,
-//         email: userInfo.email
+  const sendToServer = (userInfo) => {
+    fetch('../data/schoolList.json') // Fetch the JSON file from the public folder
+        .then(response => response)
+        .then(schoolList => {
+            // Find the school name corresponding to userInfo.hd
+            const matchedSchool = schoolList.find(school => school.hd === userInfo.hd);
+            const school = matchedSchool ? matchedSchool.name : null;
+            const userData = {
+                email: userInfo.email,
+                school: school
+            };
+            axios.post('http://localhost:3001/auth/login', userData)
+                .then(response => {
+                    console.log('User info sent to server:', response.data);
+                })
+                .catch(error => {
+                    console.error('Error sending user info:', error);
+                });
+        })
+        .catch(error => {
+            console.error('Error fetching school list:', error);
+        });
+  };
+//   const { authState, setAuthState } = useContext(AuthContext);
+
+//     const initialValues = {
+//         password: '',
+//         email: ''
 //     };
 
-//     axios.post('http://localhost:3001/users', userData)
-//         .then(response => {
-//             console.log('User info sent to server:', response.data);
-//         })
-//         .catch(error => {
-//             console.error('Error sending user info:', error);
+//     const onSubmit = (data) => {
+//         axios.post('http://localhost:3001/auth/login', data).then((res) => {
+//           if(res.data.error) alert(res.data.error);
+//           else {
+//             sessionStorage.setItem("accessToken", res.data.token);
+//             setAuthState({
+//                 nickname: res.data.nickname,
+//                 id: res.data.id,
+//                 school: res.data.school,
+//                 email: res.data.email,
+//                 status: true
+//             });
+//           }
+//           console.log(res.data);
+//           navigate('/login');
 //         });
-// };
-
-  const navigate = useNavigate();
-  const { authState, setAuthState } = useContext(AuthContext);
-
-    const initialValues = {
-        password: '',
-        email: ''
-    };
-
-    const onSubmit = (data) => {
-        axios.post('http://localhost:3001/auth/login', data).then((res) => {
-          if(res.data.error) alert(res.data.error);
-          else {
-            sessionStorage.setItem("accessToken", res.data.token);
-            setAuthState({
-                nickname: res.data.nickname,
-                id: res.data.id,
-                school: res.data.school,
-                email: res.data.email,
-                status: true
-            });
-          }
-          console.log(res.data);
-          navigate('/forum');
-        });
-    };
+//     };
 
     const validationSchema = Yup.object().shape({
       email: Yup.string().required('Email cannot be empty'),
@@ -102,7 +111,7 @@ export const Login = () => {
     <div className="Login">
       <h1>Login Page</h1>
 
-      <Formik 
+      {/* <Formik 
             initialValues={initialValues} 
             onSubmit = {onSubmit}
             validationSchema = {validationSchema}
@@ -124,9 +133,9 @@ export const Login = () => {
                 <br/><ErrorMessage name ='password'component = 'span'/><br/>
                 <button type='submit'>Login</button>
             </Form>
-        </Formik>
+        </Formik> */}
 
-      {/* <h2>React Google Login</h2>
+       <h2>React Google Login</h2>
             <br />
             <br />
             {profile ? (
@@ -141,7 +150,7 @@ export const Login = () => {
                 </div>
             ) : (
                 <button onClick={() => login()}>Sign in with Google 🚀 </button>
-            )} */}
+            )} 
     </div>
   );
 }
